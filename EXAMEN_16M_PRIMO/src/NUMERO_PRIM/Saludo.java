16e4af605489b44e06ca46cebeb3b9117c3ce0ca
Mateo <Mateo@DESKTOP-SEL1953>
2022-05-16 09:41:43 +0200

    Examen

---
+++ EXAMEN_16M_PRIMO/src/NUMERO_PRIM/Saludo.java
@@ -0,0 +1,17 @@
+package NUMERO_PRIM;
+
+import java.util.Scanner;
+
+public class Saludo {
+
+	public static void main(String[] args) {
+		Scanner ent = new Scanner(System.in);
+		int num = 0;
+
+		
+		System.out.println("Hola Usuario, soy Mateo");
+		System.out.println("Introduce un número para saber si es primo o no");
+		num = ent.nextInt();
+	
+	}
+}
---
+++ EXAMEN_16M_PRIMO/src/NUMERO_PRIM/package-info.java
@@ -0,0 +1,1 @@
+package NUMERO_PRIM;
